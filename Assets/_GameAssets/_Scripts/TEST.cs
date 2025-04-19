d0ff4eb9bf5b8fa83fae5a2c81f65e3bd0f432c1
Nguyen Gia Huy <giaminh16092003@gmail.com>
2025-04-19 23:05:08 +0700

    .

--- Assets/_GameAssets/_Scripts/TEST.cs
+++ Assets/_GameAssets/_Scripts/TEST.cs
@@ -1,16 +1,45 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class TEST : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CancellationTokenSource _cancellationTokenSource;
+
+    private void Awake()
     {
-        
+        _cancellationTokenSource = new CancellationTokenSource();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        var token = _cancellationTokenSource.Token;
+        Run(token, "go");
+        Run(token, "run");
+        Run(token, "play");
+    }
+
+    [ContextMenu("Cancel Token")]
+    public void Cancel()
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    public async UniTask Run(CancellationToken token, string name)
     {
         
+        for (int i = 0; i < 1000; i++)
+        {
+            // token.ThrowIfCancellationRequested();
+            Debug.Log($"{name} : {i}");
+            await UniTask.Delay(1000, cancellationToken: token);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _cancellationTokenSource?.Cancel();
     }
 }
